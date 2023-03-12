4d2a02ee76c1c92cf97978823e84c635b7ae288a
Arlyjr <arly.lcj@gmail.com>
2023-03-12 01:53:00 -0300

    TiposVariáveis1

--- tipos-variáveis/src/TiposVariáveis.java
+++ tipos-variáveis/src/TiposVariáveis.java
@@ -1,5 +1,15 @@
 public class TiposVariáveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
     }
 }
