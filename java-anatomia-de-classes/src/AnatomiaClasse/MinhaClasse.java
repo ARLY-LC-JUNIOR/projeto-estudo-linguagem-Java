0d3f8276ff35c69adc18352f46bfcb695c46f751
Arlyjr <arly.lcj@gmail.com>
2023-03-22 22:11:22 -0300

    AnatomiaDeClasse

---
+++ java-anatomia-de-classes/src/AnatomiaClasse/MinhaClasse.java
@@ -0,0 +1,11 @@
+package AnatomiaClasse;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.print(
+                "Helo World sejam bem vindos a anatomia de classes 1 java99 9% das nossas classes iniciarão com public class Toda classe precisa de nome exemplo MinhaClasse O nome do arquivo deve ser idêntico ao nome da classe pública Após o nome definir o corpo onde iremos compor nossas classes com atributos e métodos. isso foi recente");
+
+    }
+
+}
