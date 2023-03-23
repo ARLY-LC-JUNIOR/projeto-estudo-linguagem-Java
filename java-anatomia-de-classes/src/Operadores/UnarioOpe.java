ffcce1c6315d93039904c356c4690f71804f6606
Arlyjr <arly.lcj@gmail.com>
2023-03-22 23:39:20 -0300

    OperadoresUnario

---
+++ java-anatomia-de-classes/src/Operadores/UnarioOpe.java
@@ -0,0 +1,16 @@
+package Operadores;
+
+public class UnarioOpe {
+    public static void main(String[] args) {
+        int numero = +5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+    }
+}
