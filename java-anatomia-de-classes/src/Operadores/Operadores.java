f5890b050a38c5ab58f98e367c8add2e090c2fad
Arlyjr <arly.lcj@gmail.com>
2023-03-22 21:50:09 -0300

    Operadores

---
+++ java-anatomia-de-classes/src/Operadores/Operadores.java
@@ -0,0 +1,10 @@
+package Operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "jAVA";
+
+        System.out.println(nomeCompleto);
+
+    }
+}
