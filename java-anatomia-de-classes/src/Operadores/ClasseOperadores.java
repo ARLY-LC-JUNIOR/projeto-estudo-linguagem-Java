0c4c74ca2180f6719ac1ffa53ac269a232861e6d
Arlyjr <arly.lcj@gmail.com>
2023-04-02 21:00:07 -0300

    ClasseOperadores

---
+++ java-anatomia-de-classes/src/Operadores/ClasseOperadores.java
@@ -0,0 +1,14 @@
+package Operadores;
+
+public class ClasseOperadores {
+
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+    }
+}
